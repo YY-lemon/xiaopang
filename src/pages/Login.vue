c7f3ed76f0c46f7e3139a3a74a01054784e2b88c
YY-lemon <2712863042@qq.com>
2019-07-18 12:13:01 +0800

    登录页面布局完成

---
+++ src/pages/Login.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class='login'>
+    <div class="login-box">
+      <div class="avatar">
+      </div>
+      <el-form :model="ruleForm"
+               :rules="rules"
+               ref="ruleForm"
+               label-width="100px"
+               class="form">
+        <el-form-item prop="username">
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input v-model="ruleForm.userName"
+                    prefix-icon="iconfont icon-cedaohang-zhanghao"
+                    placeholder="账户"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="ruleForm.password"
+                    prefix-icon="iconfont icon-mima"
+                    placeholder="密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary"
+                     class="loginBtn">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+           
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+        ]
+      }
+    }
+  },
+}
+</script>
+<style lang='less' scoped>
+.login {
+  width: 100vw;
+  height: 100vh;
+  background-color: #2f4050;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-box {
+    width: 480px;
+    height: 280px;
+    background-color: #fff;
+    position: relative;
+    .avatar {
+      width: 120px;
+      height: 120px;
+      background-image: url("../assets/avatar.jpg");
+      position: absolute;
+      left: 180px;
+      top: -60px;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-sizing: border-box;
+      box-shadow: 0 1px 5px #ccc;
+    }
+    .form {
+      width: 400px;
+      padding: 0 40px;
+      margin-top: 60px;
+      /deep/ .el-form-item__content {
+        margin-left: 0 !important;
+      }
+      .loginBtn {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
+// <style lang="less">
+// .form {
+//   .el-form-item__content {
+//     margin-left: 0 !important;
+//   }
+// }
+//
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/pages/Login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    name: 'Login',
+    path: '/login',
+    component: Login
+  }]
 })
